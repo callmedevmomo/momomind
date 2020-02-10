ac3440e3987ba8b672ed67d4cdf5974736787a22
momo <callmedevmomo@gmail.com>
2020-02-10 16:24:59 +0900

    heroku set

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import socketIO from "socket.io";
 import dotenv from "dotenv";
 import logger from "morgan";
+import path from "path";
 import socketController from "./socketController";
 import events from "./events";
 
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.set("view engine", "pug");
-
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(join(__dirname, "static")));
 app.use(logger("dev"));
 app.get("/", (req, res) =>
